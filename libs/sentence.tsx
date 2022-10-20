82d69baa0e5265dee0a248453dac2881d98005c7
Enreina <enreina@gmail.com>
2022-10-20 15:33:12 +0700

    feat: randomize sentence & use useSWRImmutable

--- libs/sentence.tsx
+++ libs/sentence.tsx
@@ -1,20 +1,42 @@
-import useSWR from "swr";
+import useSWRImmutable from "swr/immutable";
 
 const WANIKANI_API_KEY = ''; //replace with WaniKani API from https://www.wanikani.com/settings/personal_access_tokens
+
+const getRandomInt = (min: number, max: number) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+};
+
 const fetcher = (url: string) => fetch(url, {
     headers: {
         'Authorization': `Bearer ${WANIKANI_API_KEY}` 
     }
-}).then(res => res.json());
-const url = 'https://api.wanikani.com/v2/subjects?types=vocabulary&levels=22';
+})
+.then(res => res.json())
+.then(data => {
+    let contextSentence: string | null = null;
+
+    // randomized sentence
+    if (data?.data && data.data.length > 0) {
+        const randomVocabIndex = getRandomInt(0, data.data.length);
+        const randomVocabContextSentences = data.data[randomVocabIndex].data['context_sentences'];
+        if (randomVocabContextSentences.length > 0) {
+            const randomSentenceIndex = getRandomInt(0, randomVocabContextSentences.length);
+            contextSentence = randomVocabContextSentences[randomSentenceIndex];
+        }
+    }
+    
+    return contextSentence;
+});
+
+const url = 'https://api.wanikani.com/v2/subjects?types=vocabulary&levels=1,2,3';
 
 export const useSentence = () => {
-    const {data, error} = useSWR(url, fetcher);
-
-    const contextSentence = data?.data[0]?.data["context_sentences"][0];
+    const {data, error} = useSWRImmutable(url, fetcher);
 
     return {
-        sentence: contextSentence,
+        sentence: data,
         isLoading: !error && !data,
         isError: error,
     };
