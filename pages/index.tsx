bcb7a689b856ebe5b8e34c2bdf3b152a0a64fdd1
Enreina <enreina@gmail.com>
2022-10-20 20:42:57 +0700

    feat: add spinner while fetching sentence

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Container from '@mui/material/Container';
+import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
@@ -11,7 +12,7 @@ import { useSentence } from '../libs/sentence';
 
 const Home: NextPage = () => {
   const [isEnglishVisible, setIsEnglishVisible] = useState<Boolean>(false);
-  const {sentence} = useSentence();
+  const {sentence, isLoading} = useSentence();
 
   const showEnglishButtonOnClick: () => void = () => {
     setIsEnglishVisible(true);
@@ -28,13 +29,22 @@ const Home: NextPage = () => {
       <Typography sx={{marginTop: 4}} variant="body2" color="text.secondary" align="center">１文ずつ</Typography>
       <Typography variant="body2" color="text.secondary" align="center">Ichi Bun Zutsu</Typography>
 
-      {sentence && 
+      
         <Paper sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 }}}>
-            <Typography component="h1" variant="h4" align="center">{sentence['ja']}</Typography>
-            {isEnglishVisible && <Typography variant="h5" align="center">{sentence['en']}</Typography>}
-            {!isEnglishVisible && <Button onClick={showEnglishButtonOnClick} variant="contained" fullWidth>英語を表示</Button>}
+          {sentence && (
+            <>
+              <Typography component="h1" variant="h4" align="center">{sentence['ja']}</Typography>
+              {isEnglishVisible && <Typography variant="h5" align="center">{sentence['en']}</Typography>}
+              {!isEnglishVisible && <Button onClick={showEnglishButtonOnClick} variant="contained" fullWidth>英語を表示</Button>}
+            </>
+          )}
+          {isLoading && (
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+              <CircularProgress />
+            </Box>
+          )}
         </Paper>
-      }
+      
     </Container>
   );
 };
