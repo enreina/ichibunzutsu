c7668b79b07476575a726f332ce1c07d0f8fb2a5
Enreina <enreina@gmail.com>
2022-11-27 07:02:08 +0700

    mark kuromoji as external server package

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    serverComponentsExternalPackages: ['kuroshiro-analyzer-kuromoji'],
+    serverComponentsExternalPackages: ['kuromoji'],
   },
 }
 
