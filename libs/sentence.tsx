78a5ad75021ce3dec7b75f233975d4bf2b76f5ef
Enreina <enreina@gmail.com>
2022-12-10 15:41:50 +0700

    refactor: put parse html options outside of rendering

--- libs/sentence.tsx
+++ libs/sentence.tsx
@@ -77,31 +77,31 @@ const KanjiFuriganaElement = ({kanji, furigana}: {kanji: string, furigana: strin
     </ruby>;
 }
 
-export const JapaneseSentenceElement = ({sentence} : {sentence: Sentence}) => {
-    const parseFuriganaOptions: HTMLReactParserOptions = {
-        replace: domNode => {
-            if (domNode instanceof Element && domNode.name === 'ruby') {
-                let kanji = "";
-                let furigana = "";
-                // find the kanji
-                const kanjiNode = domNode.children.find((node) => node.type === ElementType.Text); 
-                if (kanjiNode) {
-                    kanji = (kanjiNode as Text).data;
-                }
-
-                const furiganaWrapperNode = domNode.children.find((node) => node instanceof Element && node.name === 'rt');
-                if (furiganaWrapperNode instanceof Element) {
-                    const furiganaNode = (furiganaWrapperNode as Element).children.find((node) => node.type === ElementType.Text);
-                    if (furiganaNode) {
-                        furigana = (furiganaNode as Text).data;
-                    }
-                }
-
-                return <KanjiFuriganaElement kanji={kanji} furigana={furigana} />;
+const parseFuriganaOptions: HTMLReactParserOptions = {
+    replace: domNode => {
+        if (domNode instanceof Element && domNode.name === 'ruby') {
+            let kanji = "";
+            let furigana = "";
+            // find the kanji
+            const kanjiNode = domNode.children.find((node) => node.type === ElementType.Text); 
+            if (kanjiNode) {
+                kanji = (kanjiNode as Text).data;
             }
+
+            const furiganaWrapperNode = domNode.children.find((node) => node instanceof Element && node.name === 'rt');
+            if (furiganaWrapperNode instanceof Element) {
+                const furiganaNode = (furiganaWrapperNode as Element).children.find((node) => node.type === ElementType.Text);
+                if (furiganaNode) {
+                    furigana = (furiganaNode as Text).data;
+                }
+            }
+
+            return <KanjiFuriganaElement kanji={kanji} furigana={furigana} />;
         }
     }
+}
 
+export const JapaneseSentenceElement = ({sentence} : {sentence: Sentence}) => {
     if (sentence.furiganaHTML) {
         return <>
             {parse(sentence.furiganaHTML, parseFuriganaOptions)}
