6146cce2442bcd86194bf0b54f9dfef01ae98697
Enreina <enreina@gmail.com>
2022-12-21 18:16:41 +0700

    prioritize saved settings over system preference for dark mode

--- components/ThemeContext.tsx
+++ components/ThemeContext.tsx
@@ -10,17 +10,18 @@ const ThemeContext = createContext({
 export const ThemeContextProvider: React.FC<{children?: React.ReactNode}> = ({children}) => {
     const [savedSettings, _] = useSavedSettings();
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-    const [themeMode, setThemeMode] = useState<'light' | 'dark'>(prefersDarkMode ? 'dark' : 'light');
+    const [themeMode, setThemeMode] = useState<'light' | 'dark'>('light');
     const theme = useMemo(() => createTheme({palette: {mode: themeMode}}), [themeMode]);
     const toggleThemeMode = () => {
         setThemeMode(prevValue => prevValue === 'light' ? 'dark' : 'light');
     };
 
     useEffect(() => {
-        if (savedSettings?.isDarkModeEnabled) {
+        // saved settings has higher priority than system preference
+        if ((savedSettings?.isDarkModeEnabled === undefined && prefersDarkMode) || savedSettings?.isDarkModeEnabled) {
             setThemeMode('dark');
         }
-    }, [savedSettings?.isDarkModeEnabled]);
+    }, [savedSettings?.isDarkModeEnabled, prefersDarkMode]);
 
     return (
         <ThemeContext.Provider value={{toggleThemeMode}}>
