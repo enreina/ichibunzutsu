4821fae779b3dff124fdafc0bb989476268c786c
Enreina <enreina@gmail.com>
2023-01-01 20:06:56 +0700

    chore:add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeContextProvider } from '../components/ThemeContext'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ThemeContextProvider>
-    <Component {...pageProps} />
-  </ThemeContextProvider>
+  return <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <ThemeContextProvider>
+        <Component {...pageProps} />
+      </ThemeContextProvider>
+    </>;
 }
 
 export default MyApp
