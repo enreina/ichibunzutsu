698e989fc76cc15c2f2ce48efcd2addb7442505a
Enreina <enreina@gmail.com>
2023-02-06 13:16:28 +0700

    Extract NavigationBar as a separate component

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,79 +6,23 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useState, useEffect } from 'react';
-import {
-  AppBar,
-  CssBaseline,
-  Drawer,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  Toolbar,
-} from '@mui/material';
+import { CssBaseline } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import { useSentence } from '../lib/hooks/useSentence';
 import SettingsDialog from '../components/SettingsDialog';
 import type { SettingsType } from '../components/SettingsDialog';
 import useSavedSettings from '../lib/hooks/useSavedSettings';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import AboutDialog from '../components/AboutDialog';
-import MenuIcon from '@mui/icons-material/Menu';
 import { JapaneseSentenceElement } from '../components/JapaneseSentenceElement';
-
-type NavItemType = {
-  key: string;
-  text: string;
-  href: string;
-  as: string;
-};
+import { NavigationBar, NavItemType } from '../components/NavigationBar';
 
 const navItems: NavItemType[] = [
   { key: 'about', text: 'About', href: '?about=1', as: '/about' },
   { key: 'settings', text: 'Settings', href: '?settings=1', as: '/settings' },
 ];
 
-const NavigationLinks = ({ navItems }: { navItems: NavItemType[] }) => {
-  return (
-    <>
-      {navItems.map((item) => (
-        <Link key={item.key} href={item.href} as={item.as}>
-          <Button sx={{ textTransform: 'none' }} variant="text">
-            {item.text}
-          </Button>
-        </Link>
-      ))}
-    </>
-  );
-};
-
-const NavigationList = ({
-  navItems,
-  onNavigationItemClick,
-}: {
-  navItems: NavItemType[];
-  onNavigationItemClick: (navItem: NavItemType) => void;
-}) => {
-  return (
-    <List>
-      {navItems.map((item) => (
-        <ListItem key={item.key} disablePadding>
-          <ListItemButton
-            onClick={() => onNavigationItemClick(item)}
-            sx={{ textAlign: 'center' }}
-          >
-            <ListItemText primary={item.text} />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
-  );
-};
-
 const Home: NextPage = () => {
-  const [isDrawerMenuOpen, setIsDrawerMenuOpen] = useState<boolean>(false);
   const [isEnglishVisible, setIsEnglishVisible] = useState<boolean>(false);
   const [savedSettings, setSavedSettings] = useSavedSettings();
   const { isWaniKaniEnabled, waniKaniAPIKey } = savedSettings || {};
@@ -144,82 +88,11 @@ const Home: NextPage = () => {
       </Head>
       <CssBaseline />
 
-      <AppBar color="transparent" elevation={0} component="nav">
-        <Container sx={{ marginTop: 3 }} maxWidth="lg">
-          <Toolbar disableGutters>
-            {/* To ensure the center box is centered */}
-            <Box
-              sx={{
-                flexGrow: 0,
-                display: { xs: 'none', md: 'flex' },
-                visibility: 'hidden',
-              }}
-            >
-              <NavigationLinks navItems={navItems} />
-            </Box>
-            <Box
-              sx={{
-                flexGrow: 0,
-                display: { xs: 'flex', md: 'none' },
-                visibility: 'hidden',
-              }}
-            >
-              <IconButton>
-                <MenuIcon />
-              </IconButton>
-            </Box>
+      <NavigationBar
+        navItems={navItems}
+        navItemClickHandler={navItemClickHandler}
+      />
 
-            {/* App Name in Middle */}
-            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
-              <Typography variant="body2" color="text.secondary" align="center">
-                一文ずつ
-              </Typography>
-              <Typography variant="body2" color="text.secondary" align="center">
-                Ichi Bun Zutsu
-              </Typography>
-            </Box>
-
-            {/* Menu Icon only on mobile view */}
-            <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' } }}>
-              <IconButton
-                aria-label="open menu bar"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                color="inherit"
-                onClick={() => setIsDrawerMenuOpen(true)}
-              >
-                <MenuIcon />
-              </IconButton>
-            </Box>
-            {/* Menu on dekstop view */}
-            <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
-              <NavigationLinks navItems={navItems} />
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-
-      <Box component="nav" onClick={() => setIsDrawerMenuOpen(false)}>
-        <Drawer
-          anchor="right"
-          open={isDrawerMenuOpen}
-          variant="temporary"
-          ModalProps={{ keepMounted: true }}
-          sx={{
-            display: { xs: 'block', md: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 240 },
-          }}
-        >
-          <Box>
-            <NavigationList
-              navItems={navItems}
-              onNavigationItemClick={navItemClickHandler}
-            />
-          </Box>
-        </Drawer>
-      </Box>
-
-      <Toolbar />
       <Paper sx={{ my: 4, p: { xs: 2, md: 3 } }}>
         {sentenceIsLoaded && (
           <>
